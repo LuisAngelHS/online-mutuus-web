0ca199e94e2320b1e5aa2bb981bcf98187d87dae
Luis Ángel Hernández <luisangelhdzs97@gmail.com>
2023-12-06 13:55:07 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://LuisAngelHS.github.io/online-mutuus-web/'
+  base: 'https://LuisAngelHS.github.io/online-mutuus-web/'
 })
